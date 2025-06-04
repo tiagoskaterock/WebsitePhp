c3d18419be8855835045fda037f6fe5e07450ad0
Lucas Silva <43251181+obliteradogames@users.noreply.github.com>
2025-06-03 23:29:03 -0300

    update css
    update css

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
         <h1>Nosso Website PHP</h1>
     </header>
     <content>
-        <p>content</p>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere iaculis ipsum, in aliquam ligula elementum in. Fusce vehicula sagittis leo ut dapibus. Nunc eros ex, ullamcorper a tellus sed, euismod efficitur leo. Donec nec viverra felis. Integer at congue ligula. Nullam a nulla id ante vestibulum fermentum. Sed a diam sed purus egestas finibus sit amet vel ante. Duis fringilla ante tortor, sit amet commodo urna aliquet vel. Vivamus hendrerit ipsum quis convallis tincidunt. Morbi ut tortor blandit, semper magna vel, vehicula erat. Phasellus finibus purus id velit rutrum, ac dignissim ex pharetra. Nunc vitae varius tellus, nec lobortis odio. Aliquam erat volutpat. Nam lacinia tortor vel nisi pharetra, vitae faucibus augue pulvinar. Duis ullamcorper massa tortor, id accumsan arcu eleifend in. Nullam tellus sapien, gravida non gravida vitae, pretium sed diam.</p>
+</div>
     </content>
     <footer class="footer">
         <hr>
