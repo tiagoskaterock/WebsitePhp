37cae86d3acd13599d2255ef3485abf7bfbe584d
Lucas Silva <43251181+obliteradogames@users.noreply.github.com>
2025-06-03 23:06:22 -0300

    primeiro request
    primeiro request

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nosso Website PHP</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     <h1>Nosso Website PHP</h1>
